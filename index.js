0cb345762f89a6524a11ef73aa7bbc3e8a79cf8a
Alvin Nguyen <alvinn123@gmail.com>
2022-10-23 00:55:58 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
 //Notes : Variables here to manipulate the DOM (Show on page)
 const pokeName = document.getElementById("poke-name");
 const pokePic = document.getElementById("poke-pic");
-const pokeDesc = document.getElementById("poke-desc");
+const pokeAbilityOne = document.getElementById("poke-ability-1");
+const pokeAbilityTwo = document.getElementById("poke-ability-2");
 const prevButton = document.getElementById("pre-button");
 const nextButton = document.getElementById("next-button");
 
-// Notes : Starting to grab the data from poke API website
-const poke = fetch("https://pokeapi.co/api/v2/pokemon/6")
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => {
-    console.log(data);
-    pokeName.textContent = data["name"];
-    pokePic.src = data["sprites"]["other"]["dream_world"]["front_default"];
-    console.log(poke);
-  });
+//Notes : Starting to grab the data of Pokemon name and sprite from API
 
-//Data for description
-const pokeText = fetch("https://pokeapi.co/api/v2/pokemon-species/6")
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => {
-    pokeDesc.textContent = data["flavor_text_entries"][8]["flavor_text"];
-  });
+let id = 1;
+//<----------Adding functionality to buttons------------->
+function previous() {
+  getPokemon();
+  if (id <= 1) {
+    return;
+  } else {
+    id--;
+  }
+  console.log(id);
+}
+
+function next() {
+  getPokemon();
+  id++;
+  console.log(id);
+}
+
+function getPokemon() {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      pokeName.textContent = data["name"];
+      pokePic.src =
+        data["sprites"]["other"]["dream_world"]["front_default"] ||
+        data["sprites"]["front_default"];
+      pokeAbilityOne.textContent = data["abilities"]["0"]["ability"]["name"];
+      pokeAbilityTwo.textContent = data["abilities"]["1"]["ability"]["name"];
+    });
+}
+nextButton.addEventListener("click", next);
+prevButton.addEventListener("click", previous);
 // TODOS: Implement functionality for next and previous buttons
 //Add a party system to be able to add pokemon to party list(MAX 6)
 //Alternatively have a list of pokemon to click on; to change the pokemon displayed or search feature - dunno which I want yet
